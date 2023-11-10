261ef75df7c9635aea84a026e71679037514e68f
Mohamed Elhayany <40873107+Melhaya@users.noreply.github.com>
2023-11-10 16:19:16 +0100

    Update and rename .button.py to .submit.py

--- .submit.py
+++ .submit.py
@@ -4,21 +4,13 @@ import ipywidgets as widgets
 from IPython.display import display, Javascript
 
 
-def restart_kernel():
-    time.sleep(1.5)
-    os._exit(00)
-
-
 if __name__ == "__main__":
     # Submit button code
     link_view = widgets.Output()
 
-
     @link_view.capture(clear_output=True)
     def callback(url):
         display(Javascript(data=f'window.open("{url.tooltip}");'))
-        # restartkernel()
-
 
     button = widgets.Button(
         description="Submit Exercise",
